ffc4a02507a12fdac0cddab28577c1adc8e40b64
Jonas Birmé <jonas.birme@eyevinn.se>
2025-06-04 00:24:26 +0200

    feat: provide an HTTP api to invoke wasm app

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,13 @@ RUN curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && \
   apt-get install -y nodejs
 RUN curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash -s -- -p /runner
 COPY ./scripts ./
+COPY ./api ./api
+RUN cd /runner/api && \
+  npm install --unsafe-perm
 VOLUME /usercontent
 RUN groupadd -r runner && useradd -r -g runner runner -d /runner
 RUN chown -R runner:runner /runner
 ENV PORT=8080
 ENTRYPOINT [ "/runner/docker-entrypoint.sh" ]
 
-CMD ["/runner/bin/wasmedge", "main.wasm"]
+CMD ["node", "/runner/api/server.js"]
---
+++ api/server.js
@@ -0,0 +1,56 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+
+const fastify = Fastify({
+  logger: true
+});
+fastify.register(cors, {
+  origin: '*',
+  credentials: true,
+});  
+
+function _runWasm(reqBody) {
+  return new Promise((resolve) => {
+    const wasmedge = spawn(path.join('/runner/bin', 'wasmedge'), [
+      path.join('/usercontent', 'main.wasm')
+    ]);
+    let d = [];
+    wasmedge.stdout.on('data', (data) => {
+      d.push(data);
+    });
+    wasmedge.on('close', (code) => {
+      let buf = Buffer.concat(d);
+      resolve(buf);
+    });
+    if (!reqBody) {
+      reqBody = Buffer.from([]);
+    }
+    wasmedge.stdin.write(reqBody);
+    wasmedge.stdin.end('');
+  });
+}
+
+fastify.get('/', async (request, reply) => {
+  let buf = await _runWasm();
+  reply.send(buf.toString());
+});
+
+fastify.post('/', async (request, reply) => {
+  const typedArray = new Uint8Array(    
+    request.body.match(/[\da-f]{2}/gi).map(function (h) {
+      return parseInt(h, 16);
+    })
+  );
+  let buf = await _runWasm(typedArray);
+  reply.send(buf.toString());
+});
+
+fastify.listen({ port: process.env.PORT || 8080, host: '0.0.0.0' }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+});
